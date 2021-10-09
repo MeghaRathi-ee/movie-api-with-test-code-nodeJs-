3d3076d75b5edce30f73eed29717c4562883db75
MeghaRathi-ee <75435222+MeghaRathi-ee@users.noreply.github.com>
2021-10-09 12:05:02 +0530

    Add files via upload

---
+++ startup/db.js
@@ -0,0 +1,11 @@
+const winston = require('winston');
+const mongoose = require('mongoose');
+const config = require('config');
+
+module.exports = function(){
+    const db = config.get('db');
+    mongoose.connect(db)
+    .then(() => console.log(`Connected to ${db}...`))
+    //.then(() => winston.info('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...'));
+}
